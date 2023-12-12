5f58fd6c19446aebd8087c3d6ce7cd5be62df7c0
RIEULoic <lrieu31@outlook.fr>
2023-12-12 21:37:15 +0400

    add heroNavbar

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { lobster } from "@/app/fonts";
+import HeroNavbar from "./HeroNavbar";
 
 function Hero(params) {
   return (
@@ -17,12 +18,12 @@ function Hero(params) {
         }}
       />
       <div
-        className={`${lobster.className} text-4xl font-bold p-5 text-slate-200 border-2 border-black absolute top-0 left-0 m-10 tracking-widest`}
+        className={`${lobster.className} text-4xl font-bold p-5 text-slate-300 absolute top-0 left-0 m-10 tracking-widest`}
       >
         Louez votre tente de toit et partez à l'aventure
       </div>
-      <div className=" text-red-700 text-2xl p-5 font-bold border-2 border-black absolute bottom-0 left-1/2 transform -translate-x-1/2 my-40">
-        Hero section locabar
+      <div className=" bg-white px-2 border-2 border-gray-400 absolute bottom-0 left-1/2 -translate-x-1/2 my-40 rounded-full">
+        <HeroNavbar />
       </div>
     </div>
   );
---
+++ src/components/Home/HeroNavbar.jsx
@@ -0,0 +1,50 @@
+function HeroNavbar() {
+  return (
+    <div className=" flex ">
+      <div className="flex items-center m-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 "
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+          />
+        </svg>
+
+        <input
+          type="text"
+          placeholder="Lieu de départ"
+          className="p-2 outline-none bg-transparent border-b-2 border-gray-400"
+        />
+      </div>
+      <div className="flex flex-col m-2">
+        <div className="text-gray-400">Date de départ</div>
+        <input
+          type="date"
+          className=" outline-none bg-transparent text-black"
+        />
+      </div>
+      <div className="flex flex-col m-2">
+        <div className="text-gray-400">Date de retour</div>
+        <input
+          type="date"
+          className=" outline-none bg-transparent text-black"
+        />
+      </div>
+      <button className="m-2">Rechercher</button>
+    </div>
+  );
+}
+
+export default HeroNavbar;
