05597059e04df274a976a8176bf31cd36a6eb395
RIEULoic <lrieu31@outlook.fr>
2024-02-27 12:39:12 +0400

    github publish branch test

--- src/components/Home/HeroNavbar.jsx
+++ src/components/Home/HeroNavbar.jsx
@@ -42,7 +42,7 @@ function HeroNavbar() {
           className=" outline-none bg-transparent text-black"
         />
       </div>
-      <button className="btn btn-primary">Rechercher</button>
+      <button className="m-2">Rechercher</button>
     </div>
   );
 }
