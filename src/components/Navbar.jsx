008fdfa757ee77319f4dc9c8a26a08171c3e2a71
RIEULoic <lrieu31@outlook.fr>
2024-03-03 22:36:21 +0400

    fix navbar blur effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,76 +8,76 @@ const Navbar = () => {
 
   return (
     <div className="fixed w-full z-50 top-0  ">
-      <div className="blur-container ">
+      <div
+        className={`blur-container ${
+          isHeroNavbarExpanded ? "rounded-b-xl" : "rounded-b-full"
+        } overflow-hidden  `}
+      >
         <div className="flex justify-end gap-5 bg-gray-200/40 p-1 ">
           <div>lolove@yopmail.com</div>
           <div>06 93 65 43 21</div>
         </div>
-      </div>
-      <div
-        className={`blur-container ${
-          isHeroNavbarExpanded ? "rounded-b-xl" : ""
-        } overflow-hidden`}
-      >
-        <nav
-          className={`flex justify-around bg-gray-200/40 ${
-            isHeroNavbarExpanded ? "rounded-b-xl" : ""
-          }  `}
-        >
-          <div className="dropdown dropdown-hover">
-            <div tabIndex={0} role="button" className="btn m-1">
-              Équipement de camping
+        <div>
+          <div
+            className={`flex justify-around bg-gray-200/40 ${
+              isHeroNavbarExpanded ? "rounded-b-xl" : ""
+            }  `}
+          >
+            <div className="dropdown dropdown-hover">
+              <div tabIndex={0} role="button" className="btn m-1">
+                Équipement de camping
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Item 2</a>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Item 2</a>
-              </li>
-            </ul>
-          </div>
-          <div className="dropdown dropdown-hover">
-            <div tabIndex={0} role="button" className="btn m-1">
-              Choisir une tente
+            <div className="dropdown dropdown-hover">
+              <div tabIndex={0} role="button" className="btn m-1">
+                Choisir une tente
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Item 2</a>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Item 2</a>
-              </li>
-            </ul>
-          </div>
-          <div className="dropdown dropdown-hover">
-            <div tabIndex={0} role="button" className="btn m-1">
-              Agence de retrait
+            <div className="dropdown dropdown-hover">
+              <div tabIndex={0} role="button" className="btn m-1">
+                Agence de retrait
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Item 2</a>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Item 2</a>
-              </li>
-            </ul>
           </div>
-        </nav>
-      </div>
-      <div className="blur-container rounded-b-full overflow-hidden">
-        <div style={{ display: isHeroNavbarExpanded ? "none" : "block" }}>
-          <div className=" bg-gray-200/40 px-2   relative bottom-0 left-1/2 -translate-x-1/2  rounded-b-full">
-            <RentalForm />
+          <div>
+            <div style={{ display: isHeroNavbarExpanded ? "none" : "block" }}>
+              <div className=" bg-gray-200/40 px-2   relative bottom-0 left-1/2 -translate-x-1/2  rounded-b-full">
+                <RentalForm />
+              </div>
+            </div>
           </div>
         </div>
       </div>
