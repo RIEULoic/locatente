4ba07e91f25787737c83296147121a881b437ec9
RIEULoic <lrieu31@outlook.fr>
2024-03-22 10:35:39 +0400

    add logo and adjust footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,64 +1,66 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <footer>
       <div className="mx-10 pb-10">
         <div className="footer px-10 pt-10 rounded-t-3xl bg-violet-400/40 text-base-content">
           <aside>
-            <svg
-              width="50"
-              height="50"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              className="fill-current"
-            >
-              <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-            </svg>
-            <p>
-              ACME Industries Ltd.
+            <Image
+              src="/images/logo2.png"
+              alt="locatent logo"
+              width={150}
+              height={150}
+            />
+            <p className="leading-7 pt-2">
+              Locatente Ltd.
               <br />
-              Providing reliable tech since 1992
+              Location de vans depuis 2005
+              <br />
+              tel : 06-93-65-43-21
+              <br />
+              email : lolove@yopmail.com
             </p>
           </aside>
           <nav>
-            <h6 className="footer-title">Services</h6>
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
+            <h6 className="footer-title pb-2">Services</h6>
+            <a className="link link-hover pb-2">Agences de location</a>
+            <a className="link link-hover pb-2">
+              Véhicules aménagés à la location
+            </a>
+            <a className="link link-hover pb-2">
+              Véhicules aménagés à la vente
+            </a>
+            <a className="link link-hover pb-2">Équipement de camping</a>
           </nav>
           <nav>
-            <h6 className="footer-title">Company</h6>
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
+            <h6 className="footer-title pb-2">Société</h6>
+            <a className="link link-hover pb-2">À propos de nous</a>
+            <a className="link link-hover pb-2">Nous contacter</a>
           </nav>
           <nav>
-            <h6 className="footer-title">Legal</h6>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
+            <h6 className="footer-title pb-2">Légal</h6>
+            <a className="link link-hover pb-2">Conditions d'utilisation</a>
+            <a className="link link-hover pb-2">Politique de confidentialité</a>
+            <a className="link link-hover pb-2">Politique des cookies</a>
           </nav>
           <form>
-            <h6 className="footer-title">Newsletter</h6>
+            <h6 className="footer-title pb-2">s'abonner à la Newsletter</h6>
             <fieldset className="form-control w-80">
-              <label className="label">
-                <span className="label-text">Enter your email address</span>
-              </label>
               <div className="join">
                 <input
                   type="text"
-                  placeholder="username@site.com"
+                  placeholder="Adresse mail"
                   className="input input-bordered join-item"
                 />
-                <button className="btn btn-primary join-item">Subscribe</button>
+                <button className="btn  bg-violet-500 border-violet-600 join-item">
+                  S'inscrire
+                </button>
               </div>
             </fieldset>
           </form>
         </div>
-        <div className="footer footer-center p-10 bg-violet-400/40 text-base-content rounded-b-3xl">
+        <div className="footer footer-center px-10 pb-10 bg-violet-400/40 text-base-content rounded-b-3xl">
           <nav>
             <div className="grid grid-flow-col gap-4">
               <a>
@@ -97,7 +99,7 @@ export default function Footer() {
             </div>
           </nav>
           <aside>
-            <p>Copyright © 2024 - All right reserved by Lolove</p>
+            <p>Copyright © 2024 - Lolove</p>
           </aside>
         </div>
       </div>
