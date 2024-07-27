d2bfb9f39afb7e8c423d2cddaab0371edff4b90a
RIEULoic <lrieu31@outlook.fr>
2024-07-27 14:04:10 +0400

    finish agencies page

--- src/app/agencies/page.jsx
+++ src/app/agencies/page.jsx
@@ -2,6 +2,7 @@
 
 import { request, gql } from "graphql-request";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const page = () => {
   const [agenciesList, setAgenciesList] = useState([]);
@@ -39,16 +40,46 @@ const page = () => {
   }, []);
 
   useEffect(() => {
-    console.log(agenciesList[0]?.adress);
+    //console.log(agenciesList[0]?.adress);
   }, [agenciesList]);
   return (
     <>
-      <div className="mt-[104px] flex justify-center mb-10">
+      <div className="mt-[104px] flex justify-around mb-10 h-[770px]">
+        <div
+          className="mt-5 border-solid border-2 rounded-3xl  p-8 "
+          style={{ borderColor: "#7C3AED" }}
+        >
+          <div className="text-5xl font-lobster flex justify-center">
+            Agences Locatente
+          </div>
+          <p className="text-3xl py-5 ">
+            Découvrez nos agences à travers toute la France
+          </p>
+          <div className="max-h-[calc(100vh-350px)] overflow-auto">
+            {/* Cela utilise la fonction calc pour calculer la hauteur maximale en
+            soustrayant 350 pixels de la hauteur de la fenêtre (100vh).  */}
+            {agenciesList.map((agency) => {
+              return (
+                <div key={agency.id}>
+                  <Link href={`/agencies/${agency.id}`}>
+                    <div className="flex flex-col items-center border-t-[2px] text-xl text-gray-700  hover:bg-violet-500">
+                      <div className="text-black font-medium pt-4 pb-2">
+                        {agency.city}
+                      </div>
+                      <div>{agency.adress}</div>
+                      <div className="pb-4">{agency.tel}</div>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
         <iframe
           className="rounded-3xl mt-5"
           src="https://www.google.com/maps/d/embed?mid=12e-pBTzphlnZRJ691v_h3VxYDng1qpA&ehbc=2E312F"
-          width="800"
-          height="800"
+          width="1000"
+          height="750"
           allowFullScreen
           loading="lazy"
           title="agencies map"
